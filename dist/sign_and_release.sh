5505fb78f2da800f29838a6dac58395236cd4535
Super Phat Arrow <38541071+SuperPhatArrow@users.noreply.github.com>
2022-08-08 19:05:23 +0100

    FIX: error in location pointing to hashed release

--- dist/sign_and_release.sh
+++ dist/sign_and_release.sh
@@ -3,8 +3,8 @@ echo ">> Enter the new version to release:"
 read VERSION
 echo ">> Enter the email address of your PGP keys:"
 read EMAIL
-sha256sum ./borderwallets.html > ./borderwallets.txt
-gpg --sign --detach-sign --armor -u $EMAIL ./borderwallets.txt
-gpg --verify ./borderwallets.txt.asc
-gh release create $VERSION  -F ./releases/notes.md './dist/borderwallets.html#Entropy Grid Generator DOWNLOAD' ./borderwallets.txt ./borderwallets.txt.asc
+sha256sum ./dist/borderwallets.html > ./dist/borderwallets.txt
+gpg --sign --detach-sign --armor -u $EMAIL ./dist/borderwallets.txt
+gpg --verify ./dist/borderwallets.txt.asc
+gh release create $VERSION  -F ./releases/notes.md './dist/borderwallets.html#Entropy Grid Generator DOWNLOAD' ./dist/borderwallets.txt ./dist/borderwallets.txt.asc
 echo "Done!"
